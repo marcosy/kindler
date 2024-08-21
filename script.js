7fa359e80fa14eb739734a9ce0e56b07e22e8a28
Marcos Yedro <marcos.yedro@beamery.com>
2024-08-21 15:44:38 +0100

    Add ES5 compatibility

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 function run() {
-    getGospel("2024-08-21")
+    getGospel("2024-08-21");
 }
 
 function getGospel(dateString) {
-    httpGet(`https://publication.evangelizo.ws/SP/days/${dateString}?from=gospelComponent`)
-    .then(data => {
-        document.writeln(data.data.readings[2].text)
-    })
+    httpGet('https://publication.evangelizo.ws/SP/days/' + dateString + '?from=gospelComponent')
+    .then(function(data) {
+        document.writeln(data.data.readings[2].text);
+    });
 }
 
-async function httpGet(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Network response was not ok ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Got error while fetching:', error)
-    }
+function httpGet(url) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    try {
+                        var data = JSON.parse(xhr.responseText);
+                        resolve(data);
+                    } catch (e) {
+                        reject(e);
+                    }
+                } else {
+                    reject(new Error('Network response was not ok ' + xhr.statusText));
+                }
+            }
+        };
+        xhr.onerror = function() {
+            reject(new Error('Network request failed'));
+        };
+        xhr.send();
+    });
 }
 
-run()
+run();
